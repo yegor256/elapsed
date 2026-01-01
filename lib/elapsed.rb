2641f82fd2b6c2fecf000e0f6e19e6c9be4dc074
Yegor Bugayenko <yegor256@gmail.com>
2026-01-01 12:35:41 +0300

    year up to 2026

--- lib/elapsed.rb
+++ lib/elapsed.rb
@@ -1,6 +1,6 @@
 # frozen_string_literal: true
 
-# SPDX-FileCopyrightText: Copyright (c) 2024-2025 Yegor Bugayenko
+# SPDX-FileCopyrightText: Copyright (c) 2024-2026 Yegor Bugayenko
 # SPDX-License-Identifier: MIT
 
 require 'logger'
@@ -16,7 +16,7 @@ require 'tago'
 #  end
 #
 # Author:: Yegor Bugayenko (yegor256@gmail.com)
-# Copyright:: Copyright (c) 2024-2025 Yegor Bugayenko
+# Copyright:: Copyright (c) 2024-2026 Yegor Bugayenko
 # License:: MIT
 #
 # @param [Object] log The log to send .debug() to
